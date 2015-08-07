07d7194bbfe3a2f0b031616564d71b88c51055e8
Vogeltak <vogeltak@gmail.com>
2015-08-07 14:33:59 +0200

    Abstract functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+document.body.style.backgroundImage = 'url(https://unsplash.it/' + window.innerWidth + '/' + window.innerHeight + '/?random&blur)';
+
+var searchbox = document.getElementById('search');
+var searchbutton = document.getElementById('button-search')
+
+searchbox.focus();
+
+searchbox.onkeypress = function(e) {
+	if (e.keyCode === 13) {
+		search();
+	}
+}
+
+searchbutton.addEventListener('click', search, false);
+
+function search() {
+	var query = searchbox.value;
+	window.location = 'https://duckduckgo.com/?q=' + query;
+}
